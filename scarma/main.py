c615438855b7dc7760331358d0d1de3e32cc9bab
Nikita Serba <nikitaserba@icloud.com>
2020-06-12 02:10:59 +0300

    Add copyright header

--- scarma/main.py
+++ scarma/main.py
@@ -1,4 +1,26 @@
-from telegram.ext import Updater
+#  ____   ____
+# / ___| / ___|__ _ _ __ _ __ ___   __ _
+# \___ \| |   / _` | '__| '_ ` _ \ / _` |
+#  ___) | |__| (_| | |  | | | | | | (_| |
+# |____/ \____\__,_|_|  |_| |_| |_|\__,_|
+#
+# Yet another carma bot for telegram
+# Copyright (C) 2020 Nikita Serba. All rights reserved
+#
+# SCarma is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# SCarma is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with SCarma. If not, see <https://www.gnu.org/licenses/>.
+
+from telegram.ext import Updater, MessageHandler, Filters
 import logging
 
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
