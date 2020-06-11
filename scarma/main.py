7808253a6f998abcff5550b2d0eb45ef91c05f33
Nikita Serba <nikitaserba@icloud.com>
2020-06-12 02:10:46 +0300

    Fix some stuff

--- scarma/main.py
+++ scarma/main.py
@@ -1,11 +1,10 @@
 from telegram.ext import Updater
 import logging
 
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.DEBUG)
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
 if __name__ == "__main__":
-    updater = Updater(token='TOKEN', use_context=True)
+    updater = Updater(token='<token>', use_context=True)
     dispatcher = updater.dispatcher
 
     updater.start_polling()
-
