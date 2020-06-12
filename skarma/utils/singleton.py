ec184fee922b84bb2aef94725e7bc5087292e51a
Nikita Serba <nikitaserba@icloud.com>
2020-06-12 16:30:06 +0300

    Add singleton meta class

---
+++ skarma/utils/singleton.py
@@ -0,0 +1,38 @@
+#   ____  _  __
+#  / ___|| |/ /__ _ _ __ _ __ ___   __ _
+#  \___ \| ' // _` | '__| '_ ` _ \ / _` |
+#   ___) | . \ (_| | |  | | | | | | (_| |
+#  |____/|_|\_\__,_|_|  |_| |_| |_|\__,_|
+#
+# Yet another carma bot for telegram
+# Copyright (C) 2020 Nikita Serba. All rights reserved
+#
+# SKarma is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License.
+#
+# SKarma is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with SKarma. If not, see <https://www.gnu.org/licenses/>.
+
+
+class SingletonMeta(type):
+    """
+    Meta class for creating singleton classes
+
+    Copied from https://stackoverflow.com/questions/6760685/creating-a-singleton-in-python.
+
+    Copyright (C) 2011 Adam Forsyth (https://stackoverflow.com/users/500584/agf)
+    Original class is licensed under terms of CC BY-SA 4.0.
+    For more information see Stack Overflow Public Network Terms of Service, section 6.
+    """
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
