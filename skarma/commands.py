fafa1ca0c65bb44cb00b288a0cdd92e6c5b870b9
Nikita S <nikitaserba@icloud.com>
2020-07-04 16:19:14 +0300

    Clear errors in status command

--- skarma/commands.py
+++ skarma/commands.py
@@ -51,7 +51,7 @@ def status(update, context):
 
     number_of_errors = ErrorManager().get_number_of_errors()
     message = f"Status: Running in DEBUG mode ({'Stable' if number_of_errors == 0 else 'Unstable'})\n" \
-              f"Unexpected errors: {number_of_errors}\n" \
+              f"Unexpected errors: {number_of_errors} (/clear_errors)\n" \
               f"Logging status: " + ("logging normally\n" if len(blog.handlers) != 0 else "logging init failed\n") + \
               f"Database connection status: " + ("connected" if DBUtils().is_connected() else "disconnected (error)")
     context.bot.send_message(chat_id=update.effective_chat.id, text=message)
